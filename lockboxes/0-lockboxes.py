f1e020d918b052002c1dc35ff81d8e012869e473
KhairiTab <taboubikhairi68@gmail.com>
2025-01-04 17:40:43 +0100

    Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,9 +1,9 @@
+#!/usr/bin/python3
+
 def canUnlockAll(boxes):
     """
-    
     Args:
         boxes (list of list): List of boxes, each containing a list of keys.
-    
     Returns:
         bool: True if all boxes can be unlocked, False otherwise.
     """
@@ -13,13 +13,11 @@ def canUnlockAll(boxes):
     # unlocking while there are keys available
     while keys:
         current_key = keys.pop()
-        
+
         # Only process valid keys that lead to locked boxes
         if current_key < len(boxes) and current_key not in unlocked:
             # Unlock the box and add its keys
             unlocked.add(current_key)
             keys.update(boxes[current_key])
 
-    # Return True if the number of unlocked boxes equals the total number of boxes
     return len(unlocked) == len(boxes)
-
