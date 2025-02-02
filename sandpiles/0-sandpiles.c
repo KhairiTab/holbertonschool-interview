2bd768669299f24f8ec6e0dd8cee063ab95459af
KhairiTab <taboubikhairi68@gmail.com>
2025-02-02 16:28:31 +0100

    update cod

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -8,9 +8,9 @@ void print_grid(int grid[3][3])
 {
 int i, j;
 
-for (i = 0; i < 3; i++)
-{
-	for (j = 0; j < 3; j++)
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
 		{
 			if (j > 0)
 				printf(" ");
