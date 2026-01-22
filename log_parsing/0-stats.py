45eef8f688856da0739398b98dc904f0ef4073e9
KhairiTab <taboubikhairi68@gmail.com>
2026-01-22 09:35:52 +0100

    update file

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,39 +1,48 @@
 #!/usr/bin/python3
-"""a script that reads stdin line by line and computes metrics"""
+"""
+Task - Script that reads stdin line by line and computes metrics
+"""
+
 import sys
 
-if __name__ == "__main__":
-    status_code = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0,
-    }
-    numb_lines = 0
-    tot_size = 0
 
-    def print_statistics():
-        """a function that prints statistics of the log"""
-        print(f"File size: {tot_size}")
-        for code, count in sorted(status_code.items()):
-            if count > 0:
-                print(f"{code}: {count}")
+if __name__ == "__main__":
+    st_code = {"200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0}
+    count = 1
+    file_size = 0
+
+    def parse_line(line):
+        """ Read, parse and grab data"""
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in st_code.keys():
+                st_code[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
+
+    def print_stats():
+        """print stats in ascending order"""
+        print("File size: {}".format(file_size))
+        for key in sorted(st_code.keys()):
+            if st_code[key]:
+                print("{}: {}".format(key, st_code[key]))
 
     try:
         for line in sys.stdin:
-            numb_lines += 1
-            words = line.split(" ")
-            if len(words) > 2:
-                file_size = int(words[-1])
-                tot_size += file_size
-                code = words[-2]
-                if code in status_code:
-                    status_code[code] += 1
-                if numb_lines % 10 == 0:
-                    print_statistics()
-    finally:
-        print_statistics()
+            file_size += parse_line(line)
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
